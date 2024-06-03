787ebcb879fccf1d9f63cc967266936e41baad82
RudychikFedorV <rudychik.fedor@gmail.com>
2024-06-03 21:19:39 +0300

    gallery swiper

--- js/index.js
+++ js/index.js
@@ -124,3 +124,20 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 AOS.init();
+
+window.onload = function () {
+  const swiper = new Swiper(".swiper", {
+    // direction: 'vertical',
+    loop: true,
+    slidesPerView: 4,
+    parallax: true,
+    speed: 1000,
+    keyboard: {
+      enabled: true,
+    },
+    navigation: {
+      nextEl: "#nextEl",
+      prevEl: "#prevEl",
+    },
+  });
+};
